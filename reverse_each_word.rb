1cf2df32a7e3896ee812611c81292254057e85af
Fenty Hall <fentymhall@gmail.com>
2020-07-30 21:13:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,3 +2,7 @@ def reverse_each_word(sentence1)
   sentence2 = sentence1.split.collect {|word| word.reverse}
   sentence2.join(" ")
 end 
+
+# def reverse_each_word(sentence)
+#   sentence.split.collect {|word| word.reverse}.join(" ")
+# end
